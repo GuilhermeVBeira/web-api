436c39eec699ec16fc50bd08cd222fb1bf7f5747
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2020-10-10 11:56:01 -0300

    Add list users

--- tests/apps/clients/test_routes.py
+++ tests/apps/clients/test_routes.py
@@ -1,5 +1,6 @@
 from fastapi import status
 import pytest
+from web_app.apps.clients.models import Client as ClientModel
 from .factories import ClientFactory
 
 
@@ -21,3 +22,27 @@ def test_create_exist_email(client, client_data):
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {'detail': 'The user with this username already exists in the system.'}
+
+
+@pytest.mark.asyncio
+async def test_search_with_pagination_limit_offset(client, client_data):
+    await ClientFactory.create(email="random@email.com")
+    await ClientFactory.create(**client_data)
+
+    user_name = client_data["username"]
+    response = client.get(f"/clients/?q={user_name}&limit=1&offset=0")
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert len(data) == 1
+    assert len(await ClientModel.query.gino.all()) == 2
+
+
+def test_search_without_results(client):
+    response = client.get("/clients/?name=Some Name")
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert len(data) == 0
+
--- web_app/apps/clients/routers.py
+++ web_app/apps/clients/routers.py
@@ -1,4 +1,6 @@
+import typing
 from fastapi import APIRouter, Depends, status, HTTPException
+from web_app.pagination import Pagination
 from web_app.apps.auth.authentication import validate_token
 from web_app.apps.clients.schemas import Client
 from web_app.apps.clients.models import Client as ClientModel
@@ -26,6 +28,21 @@ async def client_create(client: Client):
         email=client.email,
     )
     return user
+
+
+@router.get(
+    "/clients/",
+    description="Endpoint to search clients",
+    response_model=typing.List[Client],
+    dependencies=[Depends(validate_token)],
+)
+async def users_search(p: Pagination = Depends()):
+    if p.q is None:
+        query = ClientModel.query.limit(p.limit).offset(p.offset)
+    else:
+        query = ClientModel.query.where(ClientModel.username == p.q).limit(p.limit).offset(p.offset)
+    users = await query.gino.all()
+    return users
 
 
 def include_router(app):
